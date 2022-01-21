9479505cb71cd82460e6cc14617a8a4adb00bb96
lee linfeng <2033211717@qq.com>
2022-01-21 15:39:38 +0800

    pass compile

--- demo2/make.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-g++ main1.cpp -o main1 -I/usr/local/include/ffmpeg/ -lavcodec -lavformat -lavutil 
-# g++ main2.cpp -o main2 -I/usr/local/include/ffmpeg/ -lavcodec -lavformat -lavutil
-
